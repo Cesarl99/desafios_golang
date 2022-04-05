4e45776ff34f8fefc053b2738abcb68d224b566b
Cesar Luiz Gomes <cesar.gomes@gruposplice.com.br>
2022-04-05 13:57:54 -0300

    atualização 05/04

--- main.go
+++ main.go
@@ -9,36 +9,44 @@ import (
 )
 
 func main() {
-	//pergunta o numero ao usuario e guarda em uma variavel
-	scanner := bufio.NewScanner(os.Stdin)
-	fmt.Println("digite um numero")
-	scanner.Scan()
-	numero := scanner.Text()
-	//---------------------------------------------------------------
-	// converter uma string em int
-	num, err := strconv.Atoi(numero)
-	if err != nil {
-		log.Fatal(err)
-	}
-	//---------------------------------------------------------------
-	{
-		fib(num)                      //executando a função do fibonacci
-		total := fib(num)             //guandando o returno da função em uma variavel
-		totals := strconv.Itoa(total) //convertendo o resultado da função(tipo int) para string e colocando em variavel e tratando o erro
+	for {
+		//pergunta o numero ao usuario e guarda em uma variavel
+		scanner := bufio.NewScanner(os.Stdin)
+		fmt.Println("qual a posição que você deseja saber o numero da sequência fibonacci(digite 1 para sair)")
+		scanner.Scan()
+		numero := scanner.Text()
+		// converter uma string em int
+		num, err := strconv.Atoi(numero)
 		if err != nil {
 			log.Fatal(err)
 		}
-		//criar arquivo e escrever
-		titulo := ("resultado do fibonnaci " + numero + ".txt") ///criar arquivo
-		file, err := os.Create(titulo)                          ///criar arquivo
-		if err != nil {
-			panic(err)
+		if num != 1 {
+			//--------------------------------------------------------------
+			//---------------------------------------------------------------
+			{
+				fib(num)                      //executando a função do fibonacci
+				total := fib(num)             //guandando o retorno da função em uma variavel
+				totals := strconv.Itoa(total) //convertendo o resultado da função(tipo int) para string e colocando em variavel e tratando o erro
+				if err != nil {
+					log.Fatal(err)
+				}
+				//criar arquivo e escrever
+				titulo := ("resultado do fibonnaci " + numero + ".txt") ///criar arquivo
+				file, err := os.Create(titulo)                          ///criar arquivo
+				if err != nil {
+					panic(err)
+				}
+				file.WriteString(totals) ///escrever no arquivo quando string
+				file.Close()             //fechar arquivo
+			}
+		} else {
+			fmt.Println("VOCE DIGITOU 1!!!!!!!!!")
+			break
 		}
-		file.WriteString(totals) ///escrever no arquivo quando string
-		file.Close()             //fechar arquivo
 	}
+}
 
-} //-------------------------------------------------------
+//-------------------------------------------------------
 //função do fibonnaci
 func fib(n int) int {
 	if n <= 1 {

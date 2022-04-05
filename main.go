4bceebcdbdc55eeac258191946dd652613e3a901
Cesar Luiz Gomes <cesar.gomes@gruposplice.com.br>
2022-04-05 11:58:28 -0300

    atualização 05/04

--- main.go
+++ main.go
@@ -8,15 +8,12 @@ import (
 	"strconv"
 )
 
-var total []int
-
 func main() {
 	//pergunta o numero ao usuario e guarda em uma variavel
 	scanner := bufio.NewScanner(os.Stdin)
 	fmt.Println("digite um numero")
 	scanner.Scan()
 	numero := scanner.Text()
-	fmt.Printf("%t\n", numero)
 	//---------------------------------------------------------------
 	// converter uma string em int
 	num, err := strconv.Atoi(numero)
@@ -25,29 +22,27 @@ func main() {
 	}
 	//---------------------------------------------------------------
 	{
+		fib(num)                      //executando a função do fibonacci
+		total := fib(num)             //guandando o returno da função em uma variavel
+		totals := strconv.Itoa(total) //convertendo o resultado da função(tipo int) para string e colocando em variavel e tratando o erro
+		if err != nil {
+			log.Fatal(err)
+		}
 		//criar arquivo e escrever
 		titulo := ("resultado do fibonnaci " + numero + ".txt") ///criar arquivo
 		file, err := os.Create(titulo)                          ///criar arquivo
 		if err != nil {
 			panic(err)
 		}
-		data := []byte("bom dia\n")           //o que vai escrever no arquivo
-		file.Write(data)                      //escrever no arquivo
-		file.WriteString("isto e uma string") ///escrever no arquivo quando string
-		file.Close()                          //fechar arquivo
+		file.WriteString(totals) ///escrever no arquivo quando string
+		file.Close()             //fechar arquivo
 	}
 
-	//-------------------------------------------------------
-	fmt.Printf("%t\n", num)
-	//fmt.Println(fib(num))
-
-}
-
-/*
+} //-------------------------------------------------------
+//função do fibonnaci
 func fib(n int) int {
 	if n <= 1 {
 		return n
 	}
 	return fib(n-1) + fib(n-2)
 }
-*/

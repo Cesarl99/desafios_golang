6e8d195d3243c3c37c95ef853e8a4e00ce8884ae
Cesar Luiz Gomes <cesar.gomes@gruposplice.com.br>
2022-04-05 09:22:39 -0300

    commit 05/04

--- main.go
+++ main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"bufio"
 	"fmt"
-	"io/ioutil"
 	"log"
 	"os"
 	"strconv"
@@ -12,24 +11,30 @@ import (
 var total []int
 
 func main() {
+	//pergunta o numero ao usuario e guarda em uma variavel
 	scanner := bufio.NewScanner(os.Stdin)
 	fmt.Println("digite um numero")
 	scanner.Scan()
 	numero := scanner.Text()
+	fmt.Printf("%t\n", numero)
+	//---------------------------------------------------------------
+	// converter uma string em int
 	num, err := strconv.Atoi(numero)
 	if err != nil {
 		log.Fatal(err)
 	}
+	//-------------------------------------------------------
 	fmt.Printf("%t\n", num)
-	fmt.Println(fib(num))
+	//fmt.Println(fib(num))
 
 }
 
+/*
 func fib(n int) int {
 	if n <= 1 {
 		return n
 	}
-	total = fib(n-1) + fib(n-2)
+	total := []byte("teste")
 	err := ioutil.WriteFile("C:/Users/cesar.gomes/Desktop/go teste/teste.txt", total, 0644)
 
 	if err != nil {
@@ -38,3 +43,4 @@ func fib(n int) int {
 	return fib(n-1) + fib(n-2)
 
 }
+*/
